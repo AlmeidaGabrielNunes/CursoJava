3c4e050b6e86c45f2d8917c34f5e744bba7e694f
Nome <almeidagabriel.nunes@gmail.com>
2023-12-10 12:35:23 -0300

    Atualizando o programa pra usar classes

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -1,29 +1,33 @@
 package application;
-import java.util.Scanner;
 import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangle;
 public class Program {
 
 	public static void main(String[] args) {
 		// Define o Locale padrão para os Estados Unidos
 		Locale.setDefault(Locale.US); 
 		Scanner sc = new Scanner(System.in);
-		double xA,xB,xC, yA,yB,yC;
+		Triangle x,y;
+		x = new Triangle();
+		y = new Triangle();
 		System.out.println("Enter the measures of triangle X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c= sc.nextDouble();
 		
-		double p = (xA +xB + xC)/2;
-		double areaX = Math.sqrt(p * (p-xA) * (p-xB)* (p-xC));
-		double py = (yA +yB + yC)/2;
-		double areaY = Math.sqrt(py * (py-yA) * (py-yB)* (py-yC));
+		double p = (x.a +x.b + x.c)/2;
+		double areaX = Math.sqrt(p * (p-x.a) * (p-x.b)* (p-x.c));
+		double py = (y.a +y.b + y.c)/2;
+		double areaY = Math.sqrt(py * (py-y.a) * (py-y.b)* (py-y.c));
 		
-		System.out.printf("Triangle X area: %.4fn", areaX);
-		System.out.printf("Triangle Y area: %.4fn", areaY);
+		System.out.printf("Triangle X area: %.4f\n", areaX);
+		System.out.printf("Triangle Y area: %.4f\n", areaY);
 		
 		if (areaX > areaY) {
 			System.out.println("The Larger area is: X");
