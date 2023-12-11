a76d06cb28f83e611a3a03820e40c9b5963b66f2
Nome <almeidagabriel.nunes@gmail.com>
2023-12-11 13:25:00 -0300

    Novo programa pra solucionar problemas de produto Aprendendo a usar atribuições de valores através daa classe.

---
+++ course/src/application/Program1.java
@@ -0,0 +1,19 @@
+package application;
+import java.util.Scanner;
+import java.util.Locale;
+import entities.Products;
+public class Program1 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Products products = new Products();
+		System.out.println("Enter product Data: ");
+		System.out.println("Name: ");
+		products.name = sc.nextLine();
+		System.out.println("Price: ");
+		products.price = sc.nextDouble();
+	}
+
+}
